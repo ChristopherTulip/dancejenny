ea50474b528aaaaf92890694b16a78f44e0188ce
Chris Tulip <Christopher.tulip@gmail.com>
2016-05-30 18:07:37 -0700

    added flash workflow for emails. Still need to write email confirmations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def post
+    # binding.pry
+    redirect_to root_path, notice: "Thanks for reaching out, you just emailed us!"
+  end
 end
